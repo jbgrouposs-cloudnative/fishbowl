58d4540e32146730eb6142408876b9d7a0118118
Shunichi Takagi <shunichi_takagi@jbat.co.jp>
2016-09-27 15:43:31 +0900

    Azure Service Fabric編完成！

---
+++ backend-servicefabric/FishBowling/WebApi1/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApi1.Controllers
+{
+    [ServiceRequestActionFilter]
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values 
+        public int Post([FromBody]ScoreObject value)
+        {
+            int total = 0;
+            foreach(List<int> list in value.Score)
+            {
+                foreach(int val in list)
+                {
+                    total = total + val;
+                }
+                
+            }
+            return total;
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
